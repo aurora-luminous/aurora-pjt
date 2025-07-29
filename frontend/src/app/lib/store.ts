5531b1624823e2db5b04ae1301662d8293ea06a4
kimbyeongnyeon <qud5252@naver.com>
2025-07-30 02:34:58 +0900

    feat: 음성 채널에서 사용할 상태 값들에 대한 reducer 정의

--- frontend/src/app/lib/store.ts
+++ frontend/src/app/lib/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import modalReducer from "../(server-setup)/hooks/useModal";
+import voiceChannelReducer from "../(servers)/store/voiceChannelSlice";
 
 export const store = configureStore({
   reducer: {
     modal: modalReducer,
+    voiceChannel: voiceChannelReducer,
   },
 });
 

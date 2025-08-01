f442c96734dde82eb6efce785f70be35f00787c4
KimSeHyeon <61528451+kim13245@users.noreply.github.com>
2025-08-02 01:50:21 +0900

    feat(#11): ERD 기반 엔티티 생성

---
+++ be/ex/src/entities/user.entity.ts
@@ -0,0 +1,52 @@
+import {
+    Entity,
+    PrimaryGeneratedColumn,
+    Column,
+    OneToOne,
+    OneToMany,
+} from 'typeorm';
+import { ApiProperty } from '@nestjs/swagger';
+import { Option } from './options.entity';
+import { ServerMember } from './server-member.entity';
+import { ProjectMember } from './project-member.entity';
+import { ChannelMember } from './channel-member.entity';
+
+@Entity('user')
+export class User {
+    @ApiProperty({ description: '사용자 기본키' })
+    @PrimaryGeneratedColumn()
+    userPK: number;
+
+    @ApiProperty({ description: '사용자 이메일', maxLength: 50})
+    @Column({ type: 'varchar', length: 50, unique: true })
+    user_email: string;
+
+    @ApiProperty({ description: '사용자 이름', maxLength: 10})
+    @Column({ type: 'varchar', length: 10 })
+    user_name: string;
+
+    @ApiProperty({ description: '비밀번호', maxLength: 20})
+    @Column({ type: 'varchar', length: 20 })
+    password: string;
+
+    @ApiProperty({ description: '삭제 여부', default: false})
+    @Column({ type: 'boolean', default: false })
+    user_is_deleted: boolean;
+
+    @ApiProperty({ description: '프로필 이미지 경로', required: false})
+    @Column({ type: 'text', nullable: true })
+    profile_image_path: string;
+
+    // 관계 설정
+    @OneToOne(() => Option, option => option.user)
+    option: Option;
+
+    @OneToMany(() => ServerMember, serverMember => serverMember.user)
+    serverMembers: ServerMember[];
+
+    @OneToMany(() => ProjectMember, projectMember => projectMember.user)
+    projectMembers: ProjectMember[];
+
+    @OneToMany(() => ChannelMember, channelMember => channelMember.user)
+    channelMembers: ChannelMember[];
+}

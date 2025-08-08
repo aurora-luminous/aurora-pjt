fc57ea9769461632fbfd1f30c49419da560d0a4e
KimSeHyeon <61528451+kim13245@users.noreply.github.com>
2025-08-09 00:32:22 +0900

    refactor(#11): 도메인 구조 변경 - 이전 파일 삭제

--- be/ex/src/domain/user_management/dto/update-user.dto.ts
+++
@@ -1,8 +0,0 @@
-import { PartialType, OmitType } from '@nestjs/swagger';
-import { CreateUserDto } from './create-user.dto';
-
-export class UpdateUserDto extends PartialType(
-  OmitType(CreateUserDto, ['user_email', 'password'] as const)
-) {
-  // user_email과 password는 수정 불가 (보안상)
-}

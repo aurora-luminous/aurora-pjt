dbb6674cd6dfc747ea211eee35d2039e5ac00d56
kimbyeongnyeon <qud5252@naver.com>
2025-08-13 13:01:33 +0900

    feat(#16): 전체 화면일 때 사이드바 사라지도록 수정

--- frontend/src/app/(servers)/layout.tsx
+++ frontend/src/app/(servers)/layout.tsx
@@ -2,6 +2,7 @@
 
 import React from "react";
 import { useServerLayout } from "./hooks/useServerLayout";
+import { useFullscreen } from "./hooks/useFullscreen";
 import { ServerHeader, ProjectSidebar, UserSidebar } from "./components";
 
 export default function ServersLayout({
@@ -25,47 +26,55 @@ export default function ServersLayout({
     toggleSidebar,
   } = useServerLayout();
 
+  const { isFullscreen } = useFullscreen();
+
   return (
     <div className="h-screen flex flex-col bg-white">
       {/* 상단 헤더 */}
-      <ServerHeader
-        serverId={serverId}
-        channelId={channelId}
-        getServerName={getServerName}
-        getChannelName={getChannelName}
-        toggleSidebar={toggleSidebar}
-        isSidebarOpen={isSidebarOpen}
-      />
+      {!isFullscreen && (
+        <ServerHeader
+          serverId={serverId}
+          channelId={channelId}
+          getServerName={getServerName}
+          getChannelName={getChannelName}
+          toggleSidebar={toggleSidebar}
+          isSidebarOpen={isSidebarOpen}
+        />
+      )}
 
       <div className="flex flex-1 bg-aurora-main">
         {/* 왼쪽+중앙: 프로젝트 목록과 채널 목록을 포함하는 사이드바 영역 */}
-        <ProjectSidebar
-          serverId={serverId}
-          projectId={projectId}
-          channelId={channelId}
-          isProjectActive={isProjectActive}
-          isProjectSelected={isProjectSelected}
-        />
+        {!isFullscreen && (
+          <ProjectSidebar
+            serverId={serverId}
+            projectId={projectId}
+            channelId={channelId}
+            isProjectActive={isProjectActive}
+            isProjectSelected={isProjectSelected}
+          />
+        )}
 
         {/* 오른쪽: 메인 콘텐츠 영역 */}
         <div className="flex-1 bg-gray-800 relative">{children}</div>
 
         {/* 사이드바 with 슬라이드 애니메이션 */}
-        <div
-          className={`bg-gray-600 flex flex-col rounded-tl-lg transition-all duration-300 ease-in-out overflow-hidden ${
-            isSidebarOpen ? "w-64 opacity-100" : "w-0 opacity-0"
-          }`}
-        >
-          <UserSidebar
-            activeTab={activeTab}
-            setActiveTab={setActiveTab}
-            onlineUsers={onlineUsers}
-            directMessages={directMessages}
-            serverId={serverId}
-            projectId={projectId}
-            isSidebarOpen={isSidebarOpen}
-          />
-        </div>
+        {!isFullscreen && (
+          <div
+            className={`bg-gray-600 flex flex-col rounded-tl-lg transition-all duration-300 ease-in-out overflow-hidden ${
+              isSidebarOpen ? "w-64 opacity-100" : "w-0 opacity-0"
+            }`}
+          >
+            <UserSidebar
+              activeTab={activeTab}
+              setActiveTab={setActiveTab}
+              onlineUsers={onlineUsers}
+              directMessages={directMessages}
+              serverId={serverId}
+              projectId={projectId}
+              isSidebarOpen={isSidebarOpen}
+            />
+          </div>
+        )}
       </div>
     </div>
   );

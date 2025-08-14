1ef915b52e533c62a7e912528c1242918171083b
이용재 <lyj3023@naver.com>
2025-08-14 22:00:51 +0900

    feat : MemberServiceImpl 추가, MemberService 오타 수정

--- spbackend/src/main/java/com/luminous/aurora/member/service/MemberService.java
+++ spbackend/src/main/java/com/luminous/aurora/member/service/MemberService.java
@@ -3,7 +3,7 @@ package com.luminous.aurora.member.service;
 public interface MemberService {
 
     // 채널 접근 권한 확인
-    boolean hasChannelAccess(Integer channelPk, Integer userPK);
+    boolean hasChannelAccess(Integer channelPk, Integer userPk);
 
     // DM방 접근 권한 확인
     boolean hasDmRoomAccess(Integer dmRoomPk, Integer userPk);

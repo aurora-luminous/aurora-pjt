a1eaa3d326e3ca8cc717fde68b5be3a2c64252f7
kimbyeongnyeon <qud5252@naver.com>
2025-08-20 01:56:29 +0900

    feat(#29): 연결 모달창 수정

--- frontend/src/app/(server-setup)/server-connect/page.tsx
+++ frontend/src/app/(server-setup)/server-connect/page.tsx
@@ -2,7 +2,6 @@
 
 import React, { useState } from "react";
 import { motion } from "framer-motion";
-import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useModal } from "../hooks/useModal";
 import AddServerModal from "../components/AddServerModal";

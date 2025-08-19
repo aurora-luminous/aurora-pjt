5086e77312c0072871cbd217fe213468fb51b4e2
KimSeHyeon <61528451+kim13245@users.noreply.github.com>
2025-08-19 14:00:11 +0900

    fix(#11): response를 api 명세대로 수정

--- be/ex/src/domain/server/dto/server-response.dto.ts
+++ be/ex/src/domain/server/dto/server-response.dto.ts
@@ -15,8 +15,28 @@ export class ServerResponseDto {
 
   @ApiProperty({ description: '서버 소유자 정보', required: false })
   ownerInfo?: {
-    userPk: number;
     userName: string;
     userEmail: string;
   };
+}
+
+// 서버 목록 조회용
+export class ServerListDto {
+  @ApiProperty({ description: '서버 URL' })
+  serverUrl: string;
+
+  @ApiProperty({ description: '서버명' })
+  serverName: string;
+}
+
+// 서버 생성 응답 DTO
+export class ServerCreateResponseDto {
+  @ApiProperty({ description: '메시지' })
+  message: string;
+}
+
+// 서버 초대 링크 응답 DTO
+export class ServerInviteLinkResponseDto {
+  @ApiProperty({ description: '초대 링크' })
+  inviteLink: string;
 }

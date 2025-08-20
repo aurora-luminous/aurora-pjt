d5f382943631c117c36fb15850cfe3767bfd5396
KimSeHyeon <61528451+kim13245@users.noreply.github.com>
2025-08-20 10:15:57 +0900

    fix(#11): 엔티티 직접적으로 접근하도록 수정

--- be/ex/src/database/database.config.ts
+++ be/ex/src/database/database.config.ts
@@ -1,7 +1,14 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { ConfigService } from '@nestjs/config';
 import { SnakeNamingStrategy } from '../config/snake-naming.strategy';
-import { join } from 'path';
+import { User } from '../domain/user/entities/user.entity';
+import { UserOption } from '../domain/user/entities/user-option.entity';
+import { Server } from '../domain/server/entities/server.entity';
+import { ServerMember } from '../domain/server/entities/server-member.entity';
+import { Project } from '../domain/project/entities/project.entity';
+import { ProjectMember } from '../domain/project/entities/project-member.entity';
+import { Channel } from '../domain/text-channel/entities/channel.entity';
+import { ChannelMember } from '../domain/text-channel/entities/channel-member.entity';
 
 export const getDatabaseConfig = (ConfigService: ConfigService): TypeOrmModuleOptions => ({
     type: 'postgres',
@@ -10,7 +17,16 @@ export const getDatabaseConfig = (ConfigService: ConfigService): TypeOrmModuleOp
     username: ConfigService.get('DB_USERNAME'),
     password: ConfigService.get('DB_PASSWORD'),
     database: ConfigService.get('DB_DATABASE'),
-    entities: [join(__dirname, '..', '**', '*.entity{.ts,.js}')],
+    entities: [
+        User,
+        UserOption,
+        Server,
+        ServerMember,
+        Project,
+        ProjectMember,
+        Channel,
+        ChannelMember
+    ],
     synchronize: ConfigService.get('NODE_ENV', 'development') === 'development',
     logging: ConfigService.get('NODE_ENV', 'development') === 'development',
     namingStrategy: new SnakeNamingStrategy(),

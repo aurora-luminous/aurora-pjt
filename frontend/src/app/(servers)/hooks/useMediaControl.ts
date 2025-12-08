b8e0abd6aa6c756af2555ee95b45381361e944ff
kimbyeongnyeon <qud5252@naver.com>
2025-12-08 23:12:18 +0900

    feat(#78): 마이크 세팅 미디어 제어 담당 훅에 추가

--- frontend/src/app/(servers)/hooks/useMediaControl.ts
+++ frontend/src/app/(servers)/hooks/useMediaControl.ts
@@ -1,11 +1,18 @@
 import { useCallback } from "react";
 import { useCamera } from "./useCamera";
 import { useLoadRedux } from "./useLoadRedux";
+import { useMike } from "./useMike";
 
 export const useMediaControl = () => {
+  // 카메라 제어
   const { cameraStream, startCamera, stopCamera } = useCamera();
   const { isVideoOn, toggleMyVideo } = useLoadRedux();
 
+  // 마이크 제어
+  const { mikeStream, startMike, stopMike } = useMike();
+  const { isMicOn, toggleMyMic } = useLoadRedux();
+
+  // 비디오 토글
   const handleToggleVideo = useCallback(() => {
     // 1. Redux 상태 먼저 업데이트
     toggleMyVideo();
@@ -18,10 +25,25 @@ export const useMediaControl = () => {
     }
   }, [isVideoOn, toggleMyVideo, startCamera, stopCamera, cameraStream]);
 
+  // 마이크 토글
+  const handleToggleMic = useCallback(() => {
+    toggleMyMic();
+
+    if (isMicOn) {
+      stopMike();
+    } else {
+      startMike();
+    }
+  }, [isMicOn, toggleMyMic, startMike, stopMike]);
+
   return {
     cameraStream,
     startCamera,
     stopCamera,
     handleToggleVideo,
+    handleToggleMic,
+    mikeStream,
+    startMike,
+    stopMike,
   };
 };

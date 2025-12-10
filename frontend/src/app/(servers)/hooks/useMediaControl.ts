fcc7077a22969eb257996ebd58568f7c1d01b5ed
kimbyeongnyeon <qud5252@naver.com>
2025-12-10 15:15:02 +0900

    feat(#80): 화면 공유 기능 redux 와 연결

--- frontend/src/app/(servers)/hooks/useMediaControl.ts
+++ frontend/src/app/(servers)/hooks/useMediaControl.ts
@@ -2,17 +2,30 @@ import { useCallback, useEffect } from "react";
 import { useCamera } from "./useCamera";
 import { useLoadRedux } from "./useLoadRedux";
 import { useMike } from "./useMike";
+import { useScreenShare } from "./useScreenShare";
 
 export const useMediaControl = () => {
   // 카메라 제어
   const { cameraStream, startCamera, stopCamera } = useCamera();
-  const { isVideoOn, toggleMyVideo, setSpeaking, currentUserId, isMicOn, toggleMyMic } =
-    useLoadRedux();
+  const {
+    isVideoOn,
+    toggleMyVideo,
+    setSpeaking,
+    currentUserId,
+    isMicOn,
+    toggleMyMic,
+    toggleMyScreenShare,
+    isScreenSharing,
+  } = useLoadRedux();
 
   // 마이크 제어
   const { mikeStream, startMike, stopMike } = useMike((isSpeking) => {
     setSpeaking(currentUserId, isSpeking);
   });
+
+  // 화면 공유 제어
+  const { screenStream, startScreenShare, stopScreenShare, changeScreenShare } =
+    useScreenShare();
   // 음성 채널 입장 시 바로 마이크 on
   useEffect(() => {
     startMike();
@@ -48,14 +61,42 @@ export const useMediaControl = () => {
     }
   }, [isMicOn, toggleMyMic, startMike, stopMike]);
 
+  // 화면 공유 토글
+  const handleToggleScreenShare = useCallback(() => {
+    // 1. Redux 상태 업데이트
+    toggleMyScreenShare();
+
+    // 2. 헌재 상태에 따라 화면공유 제어
+    if (!isScreenSharing) {
+      console.log("화면 공유 시작");
+      startScreenShare(); // 켜저 있으면 끄기
+    } else {
+      console.log("화면 공유 종료");
+      stopScreenShare(); // 꺼져 있으면 켜기
+    }
+  }, [isScreenSharing, toggleMyScreenShare, startScreenShare, stopScreenShare]);
+
+  const handdleChangeScreenShare = useCallback(() => {
+    if (!isScreenSharing) return;
+    changeScreenShare();
+  }, [isScreenSharing, changeScreenShare]);
+
   return {
     cameraStream,
     startCamera,
     stopCamera,
     handleToggleVideo,
-    handleToggleMic,
+
     mikeStream,
     startMike,
     stopMike,
+    handleToggleMic,
+
+    screenStream,
+    startScreenShare,
+    stopScreenShare,
+    handleToggleScreenShare,
+    isScreenSharing,
+    handdleChangeScreenShare,
   };
 };

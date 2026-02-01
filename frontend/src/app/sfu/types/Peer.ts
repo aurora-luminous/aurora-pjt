328007c09ec857f84247bd843681235fedf8d1f7
kimbyeongnyeon <qud5252@naver.com>
2026-02-01 21:53:03 +0900

    feat(#90): Peer 타입에 컬럼 추가

--- frontend/src/app/sfu/types/Peer.ts
+++ frontend/src/app/sfu/types/Peer.ts
@@ -1,4 +1,10 @@
+import { MediaKind } from "mediasoup-client/types";
+
 export interface Peer {
+    id: string;
+    stream: MediaStream;
+    kind: MediaKind;
     userId: number;
+    producerId: string;
     clientId: string;
 }

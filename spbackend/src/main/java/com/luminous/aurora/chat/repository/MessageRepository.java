d77f20cd522c238c99e36b4a92c2fd67f74408af
yongjae730 <dldydwo19l23@gmail.com>
2026-02-23 20:34:42 +0900

    refactor : Increase message fetch limit to 40
    Increased the limit for fetching messages by channel and DM room from 20 to 40.

--- spbackend/src/main/java/com/luminous/aurora/chat/repository/MessageRepository.java
+++ spbackend/src/main/java/com/luminous/aurora/chat/repository/MessageRepository.java
@@ -12,20 +12,20 @@ import java.util.Optional;
 public interface MessageRepository extends JpaRepository<Message, Long> {
 
     // 채널별 메시지 최신 메시지 조회 (처음 로드 시) 리미트 수 만큼 최초 로드시 보여줌, 일단 20개
-    @Query("SELECT m FROM Message m WHERE m.channelPk.channelPk = :channelPk ORDER BY m.createdAt DESC LIMIT 20")
+    @Query("SELECT m FROM Message m WHERE m.channelPk.channelPk = :channelPk ORDER BY m.createdAt DESC LIMIT 40")
     List<Message> findLatestMessagesByChannelPk(@Param("channelPk") Integer channelPk);
 
     // 채널별 이전 메시지 조회 (스크롤 시)
-    @Query("SELECT m FROM Message m WHERE m.channelPk.channelPk = :channelPk AND m.createdAt < :lastMessageTime ORDER BY m.createdAt DESC LIMIT 20")
+    @Query("SELECT m FROM Message m WHERE m.channelPk.channelPk = :channelPk AND m.createdAt < :lastMessageTime ORDER BY m.createdAt DESC LIMIT 40")
     List<Message> findOlderMessagesByChannelPk(@Param("channelPk") Integer channelPk,
                                                @Param("lastMessageTime") LocalDateTime lastMessageTime);
 
     // DM 방별 최신 메시지 조회
-    @Query("SELECT m FROM Message m WHERE m.dmRoomPk.dmRoomPk = :dmRoomPk ORDER BY m.createdAt DESC LIMIT 20")
+    @Query("SELECT m FROM Message m WHERE m.dmRoomPk.dmRoomPk = :dmRoomPk ORDER BY m.createdAt DESC LIMIT 40")
     List<Message> findLatestMessagesByDmRoomPk(@Param("dmRoomPk") Integer dmRoomPk);
 
     // DM 방별 이전 메시지 조회
-    @Query("SELECT m FROM Message m WHERE m.dmRoomPk.dmRoomPk = :dmRoomPk AND m.createdAt < :lastMessageTime ORDER BY m.createdAt DESC LIMIT 20")
+    @Query("SELECT m FROM Message m WHERE m.dmRoomPk.dmRoomPk = :dmRoomPk AND m.createdAt < :lastMessageTime ORDER BY m.createdAt DESC LIMIT 40")
     List<Message> findOlderMessagesByDmRoomPk(@Param("dmRoomPk") Integer dmRoomPk,
                                               @Param("lastMessageTime") LocalDateTime lastMessageTime);
 
@@ -49,3 +49,4 @@ public interface MessageRepository extends JpaRepository<Message, Long> {
             @Param("myUserPk") Integer myUserPk
     );
 }
+

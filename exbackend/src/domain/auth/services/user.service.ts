6ef6312741fdff819791689be881434f7edd526c
KimSeHyeon <61528451+kim13245@users.noreply.github.com>
2026-03-15 21:44:19 +0900

    refactor(#147): service 파일 구현체 분리

--- exbackend/src/domain/auth/services/user.service.ts
+++ exbackend/src/domain/auth/services/user.service.ts
@@ -1,26 +1,10 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
 import { User } from '../entities/user.entity';
-import { UserRepository } from '../repositories/user.repository';
 
-@Injectable()
-export class UserService {
-  constructor(private readonly userRepository: UserRepository) {}
-
-  async getUserByEmail(email: string): Promise<User> {
-    const user = await this.userRepository.findByEmail(email);
-    if (!user) {
-      throw new NotFoundException(
-        `이메일 ${email}을 가진 사용자를 찾을 수 없습니다`,
-      );
-    }
-    return user;
-  }
-
-  async getUserByPk(userPk: number): Promise<User> {
-    const user = await this.userRepository.findByPk(userPk);
-    if (!user) {
-      throw new NotFoundException(`사용자 ID ${userPk}를 찾을 수 없습니다`);
-    }
-    return user;
-  }
+export abstract class UserService {
+  
+  // 이메일로 유저 찾기
+  abstract getUserByEmail(email: string): Promise<User>;
+  
+  // PK로 유저 찾기
+  abstract getUserByPk(userPk: number): Promise<User>;
 }

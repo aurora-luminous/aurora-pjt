f53cef3885a080abb29940208fb3d8e22a6140d0
KimSeHyeon <61528451+kim13245@users.noreply.github.com>
2026-03-15 21:42:02 +0900

    refactor(#147): 불필요한 주석 제거

--- exbackend/src/domain/auth/repositories/user.repository.ts
+++ exbackend/src/domain/auth/repositories/user.repository.ts
@@ -4,5 +4,4 @@ export abstract class UserRepository {
   abstract findByPk(userPk: number): Promise<User | null>;
   abstract findByEmail(userEmail: string): Promise<User | null>;
 
-  // 다음 member-status 부분 추가될 예정
 }

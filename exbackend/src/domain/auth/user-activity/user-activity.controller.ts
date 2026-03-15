68afe59effa5feecc85e31ac41bcc3c5305f354a
KimSeHyeon <61528451+kim13245@users.noreply.github.com>
2026-03-15 21:46:50 +0900

    refactor(#147): 도메인 밖에 있던 유저 관련 로직 auth 도메인으로 통합

---
+++ exbackend/src/domain/auth/user-activity/user-activity.controller.ts
@@ -0,0 +1,55 @@
+import {
+  Controller,
+  Patch,
+  Param,
+  ParseIntPipe,
+  UseGuards,
+  HttpCode,
+  HttpStatus,
+  Get,
+} from '@nestjs/common';
+import {
+  ApiTags,
+  ApiOperation,
+  ApiResponse,
+  ApiBearerAuth,
+} from '@nestjs/swagger';
+import { JwtAuthGuard } from '../guards/jwt-auth.guard';
+import { CurrentUser } from '../decorators/current-user.decorator';
+import { User } from '../entities/user.entity';
+import { UserActivityService } from './user-activity.service';
+
+
+@ApiTags('user-activity')
+@Controller()
+export class UserActivityController {
+  constructor(private readonly userActivityService: UserActivityService) {}
+
+  @Get('members/me/last-channel')
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth('access-token')
+  @ApiOperation({ summary: '현재 유저의 마지막 접속 채널 정보 조회' })
+  async getLastConnectedChannel(@CurrentUser() user: User) {
+    return await this.userActivityService.getLastConnectedChannel(user.userPk);
+  }
+
+  @Patch('members/me/last-channel/:channelPk')
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth('access-token')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  @ApiOperation({ summary: '프로젝트 멤버의 마지막 접속 채널 갱신' })
+  async updateLastConnectedChannel(
+    @Param('channelPk', ParseIntPipe) channelPk: number,
+    @CurrentUser() user: User,
+  ) {
+    await this.userActivityService.updateLastConnectedChannel(user.userPk, channelPk);
+  }
+
+  @Get('my-channels')
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth('access-token')
+  @ApiOperation({ summary: '현재 유저가 참가하고 있는 모든 채널 조회' })
+  async getAllChannelsForCurrentUser(@CurrentUser() user: User) {
+    return await this.userActivityService.getAllChannelsForCurrentUser(user.userPk);
+  }
+}
---
+++ exbackend/src/domain/auth/user-activity/user-activity.module.ts
@@ -0,0 +1,24 @@
+import { Module } from "@nestjs/common";
+import { AuthModule } from "../auth.module";
+import { ProjectModule } from "../../project/project.module";
+import { TextChannelModule } from "../../text-channel/text-channel.module";
+import { UserActivityController } from "./user-activity.controller";
+import { UserActivityService } from "./user-activity.service";
+import { UserActivityServiceImpl } from "./user-activity.service.impl";
+
+@Module({
+  imports: [
+    AuthModule,
+    ProjectModule,
+    TextChannelModule,
+  ],
+  controllers: [UserActivityController],
+  providers: [
+    {
+      provide: UserActivityService,
+      useClass: UserActivityServiceImpl,
+    },
+  ],
+})
+
+export class UserActivityModule {}

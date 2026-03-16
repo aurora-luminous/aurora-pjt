892d80d2162d333fcccd2321e7c22a42dc4c363f
KimSeHyeon <61528451+kim13245@users.noreply.github.com>
2026-03-16 17:07:19 +0900

    refactor(#147): 리팩토링 된 모듈을 불러오도록 수정

--- exbackend/src/domain/user/user.module.ts
+++ exbackend/src/domain/user/user.module.ts
@@ -1,16 +1,27 @@
-  import { Module } from '@nestjs/common';
-  import { TypeOrmModule } from '@nestjs/typeorm';
-  import { User } from './entities/user.entity';
-  import { UserOption } from './entities/user-option.entity';
-  import { UserService } from './services/user.service';
-  import { UserController } from './controllers/user.controller';
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { UserOption } from './entities/user-option.entity';
+import { UserService } from './services/user.service';
+import { UserServiceImpl } from './services/serviceImpl/user.service.impl';
+import { UserRepository } from './repositories/user.repository';
+import { TypeOrmUserRepository } from './repositories/repositoryImpl/typeorm-user.repository'
 
-  @Module({
-    imports: [
-      TypeOrmModule.forFeature([User, UserOption]),
-    ],
-    providers: [UserService],
-    controllers: [UserController],
-    exports: [UserService],
-  })
-  export class UserModule {}
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([User, UserOption]),
+  ],
+  controllers: [],
+  providers: [
+    {
+      provide: UserService,
+      useClass: UserServiceImpl,
+    },
+    {
+      provide: UserRepository,
+      useClass: TypeOrmUserRepository,
+    },
+  ],
+  exports: [UserService], 
+})
+export class UserModule {}

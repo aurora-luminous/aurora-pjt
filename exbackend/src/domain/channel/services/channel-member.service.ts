800434d6abb84bae7fe4d442c6927dacd1f7194f
KimSeHyeon <61528451+kim13245@users.noreply.github.com>
2026-03-20 22:18:39 +0900

    refactor(#147): 채널 서비스 파일들 엔티티 별로 통합(추상체)

---
+++ exbackend/src/domain/channel/services/channel-member.service.ts
@@ -0,0 +1,35 @@
+import { ChannelMemberDto, RemoveFromChannelDto, BulkInviteToChannelDto } from "../dto";
+
+export abstract class ChannelMemberService{
+
+  // 멤버 초대
+  abstract inviteUsersToChannel(bulkInviteDto: BulkInviteToChannelDto): Promise<{ message: string }>;
+
+  // 공개 채널 참가
+  abstract joinPublicChannel(channelPk: number, userPk: number): Promise<ChannelMemberDto>;
+
+  // 채널 멤버 목록 조회
+  abstract getChannelMembers(channelPk: number, requestUserPk: number): Promise<ChannelMemberDto[]>;
+
+  // 멤버 내보내기 (강퇴)
+  abstract removeUserFromChannel(removeDto: RemoveFromChannelDto): Promise<void>;
+
+  // 채널 나가기
+  abstract leavChannel(channelPk: number, userPk: number): Promise<{ message: string }>;
+
+  //멤버 차단 및 해제
+  abstract banUserFromChannel(channelPk: number, targetUserPk: number, adminUserPk: number): Promise<void>;
+  abstract unbanUserFromChannel(channelPk: number, targetUserPk: number, adminUserPk: number): Promise<ChannelMemberDto>;
+
+  // 멤버 권한 변경
+  abstract updateMemberRole(
+    channelPk: number,
+    targetUserPk: number,
+    newRole: string,
+    adminUserPk: number
+  ): Promise<{ message: string }>;
+
+  // 멤버 음소거 토글(미구현)
+  abstract toggleMemberMute(channelPk: number, targetUserPk: number, adminUserPk: number): Promise<ChannelMemberDto>;
+
+}

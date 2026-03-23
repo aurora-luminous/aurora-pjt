9b05525076bf8074df675a87351e2fb2621e1e05
이용재 <lyj3023@naver.com>
2026-03-23 17:42:46 +0900

    feature(#167) : 채널 일괄 uunread 조회용 요청/응답 DTO 생성

---
+++ spbackend/src/main/java/com/luminous/aurora/chat/dto/ChannelUnreadRequest.java
@@ -0,0 +1,13 @@
+package com.luminous.aurora.chat.dto;
+
+import lombok.*;
+
+import java.util.List;
+
+// Express -> Spring 으로 오는 유저별 채널 목록
+@Getter
+@NoArgsConstructor
+public class ChannelUnreadRequest {
+    private List<Integer> channelPks;
+    private Integer userPk;
+}

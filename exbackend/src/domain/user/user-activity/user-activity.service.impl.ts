7064d818b86ca238a6e524729e63bf537ce5e3db
KimSeHyeon <61528451+kim13245@users.noreply.github.com>
2026-04-07 16:40:12 +0900

    refactor(#147): 메소드 명 변경

--- exbackend/src/domain/user/user-activity/user-activity.service.impl.ts
+++ exbackend/src/domain/user/user-activity/user-activity.service.impl.ts
@@ -1,24 +1,24 @@
 import { Injectable } from '@nestjs/common';
 import { UserActivityService } from './user-activity.service';
-import { ProjectMemberUpdateService } from '../../project/services/project-member-update.service';
+import { ProjectMemberService } from '../../project/services/project-member.service';
 import { ChannelService } from 'src/domain/channel/services/channel.service';
 
 @Injectable()
 export class UserActivityServiceImpl implements UserActivityService {
   constructor(
-    private readonly projectMemberUpdateService: ProjectMemberUpdateService,
+    private readonly projectMemberService: ProjectMemberService,
     private readonly ChannelService: ChannelService
     ,
   ) {}
 
   async getLastConnectedChannel(userPk: number) {
-    return await this.projectMemberUpdateService.getLastConnectedChannelInfo(
+    return await this.projectMemberService.getLastConnectedChannelInfo(
       userPk,
     );
   }
 
   async updateLastConnectedChannel(userPk: number, channelPk: number) {
-    await this.projectMemberUpdateService.updateLastConnectedChannel(
+    await this.projectMemberService.updateLastConnectedChannel(
       userPk,
       channelPk,
     );

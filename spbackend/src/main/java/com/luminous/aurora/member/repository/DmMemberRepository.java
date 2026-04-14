597c880724577049d448f5a06d6e8add9feec0ee
이용재 <lyj3023@naver.com>
2026-04-14 16:03:52 +0900

    feat(#182): 두 유저 간 기존 DM방 조회 쿼리 추가

--- spbackend/src/main/java/com/luminous/aurora/member/repository/DmMemberRepository.java
+++ spbackend/src/main/java/com/luminous/aurora/member/repository/DmMemberRepository.java
@@ -1,5 +1,6 @@
 package com.luminous.aurora.member.repository;
 
+import com.luminous.aurora.dmroom.entity.DmRoom;
 import com.luminous.aurora.member.entity.DmMember;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -27,4 +28,15 @@ public interface DmMemberRepository extends JpaRepository<DmMember, Integer> {
 
     // DM 방의 모든 멤버 조회
     List<DmMember> findByDmRoom_DmRoomPk(Integer dmRoomPk);
+
+    /**
+     * 두 유저가 이미 같은 DM 방에 있는지 확인
+     * - DmMember 테이블에서 두 유저가 동일한 DmRoom에 속해있는지 JOIN으로 조회
+     * - DM방 생성 시 중복 방지용 (이미 존재하면 409 Conflict)
+     */
+    @Query("SELECT dm1.dmRoom FROM DmMember dm1 " +
+            "JOIN DmMember dm2 ON dm1.dmRoom = dm2.dmRoom " +
+            "WHERE dm1.user.userPk = :userPk1 AND dm2.user.userPk = :userPk2")
+    Optional<DmRoom> findExistingDmRoom(@Param("userPk1") Integer userPk1,
+                                        @Param("userPk2") Integer userPk2);
 }

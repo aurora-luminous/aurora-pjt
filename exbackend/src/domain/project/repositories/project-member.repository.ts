22749f007574d4fe1a0de012b82170c6353846ff
KimSeHyeon <61528451+kim13245@users.noreply.github.com>
2026-04-15 01:23:48 +0900

    refactor(#179): 서버 나가기 로직을 위한 프로젝트 멤버 리포지토리 메서드 추가

--- exbackend/src/domain/project/repositories/project-member.repository.ts
+++ exbackend/src/domain/project/repositories/project-member.repository.ts
@@ -1,5 +1,6 @@
 import { ProjectMember } from '../entities/project-member.entity';
 import { MemberStatus } from "src/common/enums";
+import { EntityManager } from 'typeorm';
 
 export abstract class ProjectMemberRepository {
   // 프로젝트 멤버 조회
@@ -16,9 +17,6 @@ export abstract class ProjectMemberRepository {
   // 프로젝트 참가 중인 멤버 목록 조회
   abstract findMembersByProject(projectPk: number): Promise<ProjectMember[]>;
 
-  // 유저가 참가 중인 프로젝트들의 멤버 조회
-  abstract findActiveMemberByUser(userPk: number): Promise<ProjectMember[]>;
-
   // 프로젝트 멤버 저장(생성, 권한 변경 등의 업데이트)
   abstract save(member: Partial<ProjectMember>): Promise<ProjectMember>;
 
@@ -34,4 +32,10 @@ export abstract class ProjectMemberRepository {
 
   // 유저가 마지막으로 접속한 채널 조회
   abstract findLastConnectedWithServer(userPk: number): Promise<ProjectMember | null>;
+
+  // 유저가 가입한 서버에서 모든 프로젝트 나가기 처리
+  abstract deactivateUserInServer(manager: EntityManager, serverPk: number, userPk: number): Promise<void>;
+
+  // 특정 서버의 모든 프로젝트 멤버 나가기 처리
+  abstract deactivateAllByServer(manager: EntityManager, serverPk: number): Promise<void>;
 }

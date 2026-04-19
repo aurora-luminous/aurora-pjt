db2a5cd6512cf543d142d9a29343aac1563066e2
이용재 <lyj3023@naver.com>
2026-04-20 03:49:14 +0900

    feat(#194) : DM around Message 조회 Controller 메서드 추가

--- spbackend/src/main/java/com/luminous/aurora/chat/controller/ChatController.java
+++ spbackend/src/main/java/com/luminous/aurora/chat/controller/ChatController.java
@@ -52,8 +52,8 @@ public class ChatController {
      */
     @GetMapping("/channel/{channelPk}/messages/around")
     public ResponseEntity<MessageListResponse> getAroundChannelMessages(@PathVariable Integer channelPk,
-                                                                       @RequestParam Long messagePk,
-                                                                       @CookieValue("access_token") String jwtToken) {
+                                                                        @RequestParam Long messagePk,
+                                                                        @CookieValue("access_token") String jwtToken) {
         MessageListResponse response = chatService.getAroundMessage(channelPk, messagePk, jwtToken);
         log.info("채널 around 메시지 조회: channelPk = {}, messagePk = {}, count = {}",
                 channelPk, messagePk, response.getMessages().size());
@@ -79,6 +79,19 @@ public class ChatController {
     public ResponseEntity<MessageListResponse> getOlderDmMessages(@PathVariable Integer dmRoomPk, @RequestParam LocalDateTime lastMessageTime, @CookieValue("access_token") String jwtToken) {
         MessageListResponse response = chatService.getOlderDmMessage(dmRoomPk, lastMessageTime, jwtToken);
         log.info("DM방 이전 메시지 조회 성공: dmRoomPk={}, messageCount={}, lastMessageTime={}", dmRoomPk, response.getMessages().size(), lastMessageTime);
+        return ResponseEntity.ok(response);
+    }
+
+    /**
+     * DM around 메시지 조회
+     * GET /api/jv/chat/dm/{dmRoomPk}/messages/around?messagePk=
+     */
+    @GetMapping("/dm/{dmRoomPk}/messages/around")
+    public ResponseEntity<MessageListResponse> getAroundDmMessages(@PathVariable Integer dmRoomPk,
+                                                                   @RequestParam Long messagePk,
+                                                                   @CookieValue("access_token") String jwtToken) {
+        MessageListResponse response = chatService.getAroundDmMessage(dmRoomPk, messagePk, jwtToken);
+        log.info("DM around 메시지 조회 성공: dmRoomPk = {}, messagePk = {}, count = {}", dmRoomPk, messagePk, response.getMessages().size());
         return ResponseEntity.ok(response);
     }
 }

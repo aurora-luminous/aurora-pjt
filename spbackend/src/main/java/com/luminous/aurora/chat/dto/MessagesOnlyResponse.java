0d3665fd9d94617d4212d7159ad14467bc23d7fb
이용재 <lyj3023@naver.com>
2026-05-03 14:29:03 +0900

    feat(#209) : older 메시지 목록 전용 MessageOnlyResponse 추가

---
+++ spbackend/src/main/java/com/luminous/aurora/chat/dto/MessagesOnlyResponse.java
@@ -0,0 +1,16 @@
+package com.luminous.aurora.chat.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class MessagesOnlyResponse {
+    private List<MessageResponse> messages;
+}
